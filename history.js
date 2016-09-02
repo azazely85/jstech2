51610715251f8805f5330db8eb657f3146161733
Yaroslav Cherednyk <azazely85@gmail.com>
2016-09-02 12:47:03 +0300

    BOM history length, history back, history forward

---
+++ history.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Yaroslav Cherednyk on 02.09.2016.
+ */
+clg(window.history.length);//count visited pages
+//window.history.back();// back page
+//window.history.forward();//forward page
