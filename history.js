c03a1b0c76212995bc66438bd7654d14ed76ea29
Yaroslav Cherednyk <azazely85@gmail.com>
2016-09-02 14:01:57 +0300

    BOM history go

--- history.js
+++ history.js
@@ -4,3 +4,4 @@
 clg(window.history.length);//count visited pages
 //window.history.back();// back page
 //window.history.forward();//forward page
+//window.history.go(5);//go back or forward on parameter page
